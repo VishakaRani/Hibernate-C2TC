ba4992c391bb0173cf9d2b32c9299074fbd7d96e
VISHAKA RANI <VISHAKA RANI@user>
2022-09-08 14:35:32 +0530

    first commit

---
+++ github.com/VishakaRani/Hibernate-C2TC.git/OneToOneAssociationBiDirection/src/com/cg/jpastart/entities/Address.java
@@ -0,0 +1,37 @@
+ package com.cg.jpastart.entities;
+	private String zipCode;
+@OneToOne(mappedBy="address")
+private Student student;
+public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+	//	this.addressId = addressId;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	
+}
