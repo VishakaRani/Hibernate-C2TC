7f17e151d13d0b48309381bb7164de5dc0dee6a5
VISHAKA RANI <VISHAKA RANI@user>
2022-09-08 14:36:09 +0530

    first commit

---
+++ github.com/VishakaRani/Hibernate-C2TC.git/OneToOneUniDirAssociation/src/com/tns/assocoation/enities/Address.java
@@ -0,0 +1,25 @@
+package com.tns.assocoation.enities;
+
+public class Address {
+
+	public void setStreet(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCity(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setState(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPincode(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ github.com/VishakaRani/Hibernate-C2TC.git/OneToOneUniDirAssociation/src/com/tns/assocoation/enities/Student.java
@@ -0,0 +1,15 @@
+package com.tns.assocoation.enities;
+
+public class Student {
+
+	public void setSname(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAddress(Address homeAddress) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
